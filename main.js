048fa5b4b1a212fabfa3e6327a57903442051289
Brendon Kieser <brendon.kieser@gmail.com>
2021-06-15 05:54:04 -0700

    Only need to add type matchups

--- main.js
+++ main.js
@@ -1,16 +1,113 @@
-var pokemon;
-var types;
+let pokemon;
+let types;
 
-//Load the character 
-var pokemon_promise = $.getJSON('https://raw.githubusercontent.com/Brendon-K/pokemon-info/main/pokemon.json', function(data) {
+// load the JSON files 
+let pokemon_promise = $.getJSON('https://raw.githubusercontent.com/Brendon-K/pokemon-info/main/pokemon.json', function(data) {
   pokemon = data;
 });
 
-var types_promise = $.getJSON('https://raw.githubusercontent.com/Brendon-K/pokemon-info/main/pokemon_types.json', function(data) {
+let types_promise = $.getJSON('https://raw.githubusercontent.com/Brendon-K/pokemon-info/main/pokemon_types.json', function(data) {
   types = data;
 });
 
 
 $.when(pokemon_promise, types_promise).done(function() {
+  let num_pokemon = pokemon.length;
 
+  // add all pokemon to drop down
+  let drop_down = document.getElementById("pokemon");
+  for (let i = 0; i < num_pokemon; ++i) {
+    let selection = document.createElement("option");
+    selection.text = pokemon[i]['name'];
+    drop_down.add(selection);
+  }
 });
+
+function load_stats() {
+  // get pokemon name
+  let pokemon_name = document.getElementById("pokemon").value;
+
+  // do nothing if the default selection is picked
+  if (pokemon_name == "Select a Pokémon") return;
+
+  // find the JSON entry for the selected pokemon
+  let index = pokemon.findIndex(function(item, i){
+    return item.name === pokemon_name
+  });
+  let selected_pokemon = pokemon[index];
+
+  // add pokemon title info
+  document.getElementById('pokemon-number').innerHTML = selected_pokemon['number'];
+  document.getElementById('pokemon-name').innerHTML = selected_pokemon['name'];
+
+  // add pokemon type info
+  let num_types = selected_pokemon['type'].length;
+  let type1 = selected_pokemon['type'][0];
+  let type2 = num_types > 1 ? selected_pokemon['type'][1] : "";
+
+  document.getElementById('pokemon-type').innerHTML = type1;
+  if (num_types > 1) {
+    document.getElementById('pokemon-type').innerHTML += '<br>' + type2;
+  }
+
+  // add pokemon matchup info
+  let matchups = new Object();
+  matchups.Normal = 1;
+  matchups.Fire = 1;
+  matchups.Water = 1;
+  matchups.Electric = 1;
+  matchups.Grass = 1;
+  matchups.Ice = 1;
+  matchups.Fighting = 1;
+  matchups.Poison = 1;
+  matchups.Ground = 1;
+  matchups.Flying = 1;
+  matchups.Psychic = 1;
+  matchups.Bug = 1;
+  matchups.Rock = 1;
+  matchups.Ghost = 1;
+  matchups.Dragon = 1;
+  matchups.Dark = 1;
+  matchups.Steel = 1;
+  matchups.Fairy = 1;
+  for (let i = 0; i < num_types; ++i) {
+
+  }
+
+  // add pokemon stats
+  document.getElementById('total').innerHTML = selected_pokemon['total'];
+  document.getElementById('hp').innerHTML = selected_pokemon['hp'];
+  document.getElementById('attack').innerHTML = selected_pokemon['attack'];
+  document.getElementById('defense').innerHTML = selected_pokemon['defense'];
+  document.getElementById('sp-attack').innerHTML = selected_pokemon['sp.attack'];
+  document.getElementById('sp-defense').innerHTML = selected_pokemon['sp.defense'];
+  document.getElementById('speed').innerHTML = selected_pokemon['speed'];
+
+  // add pokemon evolutions
+  if (selected_pokemon['prev_evo'] !== null) {
+    // find the JSON entry for the previous evolution
+    let index = pokemon.findIndex(function(item, i){
+      return item.number === selected_pokemon['prev_evo']
+    });
+    let evolution = pokemon[index];
+    document.getElementById('previous-evo').innerHTML = evolution['number'] + ' ' + evolution['name'];
+  } else {
+    document.getElementById('previous-evo').innerHTML = '';
+  }
+
+  document.getElementById('next-evo').innerHTML = '';
+  if (selected_pokemon['next_evo'] !== [null]) {
+    let num_evos = selected_pokemon['next_evo'].length;
+    for (let i = 0; i < num_evos; ++i) {
+      // find the JSON entry for the next evolution
+      let index = pokemon.findIndex(function(item, j){
+        return item.number === selected_pokemon['next_evo'][i]
+      });
+      let evolution = pokemon[index];
+      if (i > 0) document.getElementById('next-evo').innerHTML += '<br>';
+      document.getElementById('next-evo').innerHTML += evolution['number'] + ' ' + evolution['name'];
+    }
+  } else {
+    document.getElementById('next-evo').innerHTML = '';
+  }
+}
